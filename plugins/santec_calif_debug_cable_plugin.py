c522658ba0712d9c84b5fccf35c66eaf7a25f182
JOrosco2 <JOrosco2@gmail.com>
2025-07-09 13:52:40 -0700

    Initial release with tested plugin:
    Tested plugin with reading/writing to PLR register.

---
+++ plugins/santec_calif_debug_cable_plugin.py
@@ -0,0 +1,63 @@
+from plugin_system.base_device import CLIInterface
+import serial
+import serial.tools.list_ports
+import time
+
+plugin_info = {
+    "name":"Santec Debug Cable",
+    "type":"CLI_cable",
+    "class":"Santec_Debug"
+}
+
+class Santec_Debug(CLIInterface):
+    @classmethod
+    def can_connect(cls):
+        ports = serial.tools.list_ports.comports()
+        if len(ports) == 1:
+            print(f"Found {ports[0].device} - {ports[0].description}")
+            conn = serial.Serial(ports[0].device, baudrate=115200,timeout=1)
+            return (ports[0].device,conn)
+        elif len(ports) > 1:
+            print(f"Found the following Serial Devices:")
+            for i in ports:
+                print(f"{i.device} - {i.description}")
+            port = input(f"Which serial port would you like to use (enter name i.e. COM3): ")
+            while "COM" not in port:
+                port = input(f"ERROR! Please enter one of the ports listed above: ")
+                conn = serial.Serial(port,baudrate=115200,timeout=1)
+                return(port,conn)
+        else:
+            return None #No serial ports connected 
+
+    def __init__(self,conn):
+        self.conn = conn
+   
+    def connect(self):
+        pass
+
+    def write_reg(self,reg = "", data = ""):
+        self.conn.write(("writereg "+reg+" "+data+"\r\n").encode())
+        time.sleep(0.1)
+        resp = self.conn.read_all().decode(errors="ignore") #deal with echo
+
+    
+    """
+    Reads a register value, returns the response with the command echo and '$' stripped
+    The caller needs to deal with the result (formatting/bit shifting/etc)
+    """
+    def read_reg(self, reg = ""):
+        cmd = "readreg " + reg
+        self.conn.write((cmd+"\r\n").encode())
+        time.sleep(0.1)
+        resp = self.conn.read_all().decode(errors="ignore")
+        lines = resp.splitlines()
+        #format the response so only the value is given
+        clean_lines = [line.strip() for line in lines if line.strip() and not line.strip().startswith(cmd) and line.strip() != "$"]
+        if clean_lines:
+            return clean_lines[0]
+        else:
+            return "No data received"
+
+    def read_port(self):
+        return self.conn.port
+
---
+++ tests/test_santec_calif_debug_cable.py
@@ -0,0 +1,67 @@
+import pytest
+from plugins.santec_calif_debug_cable_plugin import Santec_Debug
+
+port, conn = Santec_Debug.can_connect()
+plugin = Santec_Debug(conn)
+
+def test_can_connect():
+   port = plugin.read_port()
+   assert isinstance(port,str)
+   print(f"\n***********"+port+"***********\n")
+
+def test_update_sequence():
+   get_response()
+   update_reg("src.hp.plr","0x0032")
+   update_reg("src.hp.save", "0x00")
+   get_response()
+   update_reg("src.hp.plr","0x0132")
+   update_reg("src.hp.save","0x01")
+   get_response()
+   update_reg("src.hp.plr","0x0091")
+   update_reg("src.hp.save","0x00")
+   update_reg("src.hp.plr","0x01A1")
+   update_reg("src.hp.save","0x01")
+   get_response()
+
+def update_reg(reg: str, val: str):
+   plugin.write_reg(reg,val)
+   
+def get_response():  
+   resp = plugin.read_reg("src.hp.plr")
+   #assert isinstance(resp,str)
+   print(f"\nPLR:***********"+resp+"***********\n")
+   int_resp = int(resp)
+   hex_resp = hex(int_resp)
+   bin_resp = bin(int_resp)
+   print(f"PLR_HEX:***********"+str(hex_resp)+"***********")
+   print(f"PLR_BIN:***********"+str(bin_resp)+"***********")
+
+   ch1_hex_resp = int_resp & 0xFF
+   ch2_hex_resp = (int_resp >> 8) & 0xFF
+   print(f"PLR_HEX CH1:***********"+str(hex(ch1_hex_resp))+"***********")
+   print(f"PLR_HEX CH2:***********"+str(hex(ch2_hex_resp))+"***********")
+   print(f"PLR_DEC CH1:***********"+str((ch1_hex_resp))+"***********")
+   print(f"PLR_DEC CH2:***********"+str((ch2_hex_resp))+"***********")
+
+"""
+   resp = plugin.read_reg("src.hp.vtec")
+   print(f"VTEC:***********"+resp+"***********")
+   int_resp = int(resp)
+   hex_resp = hex(int_resp)
+   bin_resp = bin(int_resp)
+   print(f"\nVTEC_HEX:***********"+str(hex_resp)+"***********\n")
+   print(f"\nVTEC_BIN:***********"+str(bin_resp)+"***********\n")
+
+   ch1_hex_resp = int_resp & 0xFFFF
+   ch2_hex_resp = (int_resp >>  16) & 0xFFFF
+   print(f"\nVTEC_HEX CH1:***********"+str(hex(ch1_hex_resp))+"***********\n")
+   print(f"\nVTEC_HEX CH2:***********"+str(hex(ch2_hex_resp))+"***********\n")
+   print(f"\nVTEC_DEC CH1:***********"+str((ch1_hex_resp))+"***********\n")
+   print(f"\nVTEC_DEC CH2:***********"+str((ch2_hex_resp))+"***********\n")
+"""
+   
+   
+
+
+
+    
