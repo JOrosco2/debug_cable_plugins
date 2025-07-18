35c6859c72b311491e639ae5d50b3e329630399c
JOrosco2 <JOrosco2@gmail.com>
2025-07-18 10:55:03 -0700

    Updated read_reg to filter out another possible erroneous cli response

--- debug_cable_plugins/plugins/santec_calif_debug_cable_plugin.py
+++ debug_cable_plugins/plugins/santec_calif_debug_cable_plugin.py
@@ -52,7 +52,7 @@ class Santec_Debug(CLIInterface):
         resp = self.conn.read_all().decode(errors="ignore")
         lines = resp.splitlines()
         #format the response so only the value is given
-        clean_lines = [line.strip() for line in lines if line.strip() and not line.strip().startswith(cmd) and line.strip() != "$" and line.strip() != "<CLI INITIALIZED>" and line.strip() != f"$ {cmd}"]
+        clean_lines = [line.strip() for line in lines if line.strip() and not line.strip().startswith(cmd) and line.strip() != "$" and line.strip() != "<CLI INITIALIZED>" and line.strip() != f"$ {cmd}" and line.strip() != "$ <CLI INITIALIZED>"]
         if clean_lines:
             #if tehre is more than 1 value in the response return them all, otherwise just return the single index (not as an array)
             #programs that use this plugin will need to filter through the responses properly
